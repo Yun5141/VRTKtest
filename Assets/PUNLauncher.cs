a16d047d007d5eae87902ca18c316d9e82bc6a15
Yun5141 <yun.fang.17@ucl.ac.uk>
2020-09-16 20:42:43 +0800

    first commit

---
+++ Assets/PUNLauncher.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PUNLauncher : MonoBehaviourPunCallbacks
+{
+
+    GameObject Player1;
+    GameObject Player2;
+
+    private void Awake()
+    {
+
+        PhotonNetwork.ConnectUsingSettings();
+        Debug.Log("GameInfo: Connected using settings");
+
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinOrCreateRoom("Room", new Photon.Realtime.RoomOptions() { MaxPlayers = 2 }, default);
+        Debug.Log("GameInfo: Joined Room");
+
+    }
+
+    public override void OnJoinedRoom()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            Player1 = PhotonNetwork.Instantiate("ybot", new Vector3(3, 0, 0), Quaternion.identity, 0);
+            Debug.Log("GameInfo: Instantiate Player 1 Successfully");
+        }
+        else
+        {
+            Player2 = PhotonNetwork.Instantiate("ybot", new Vector3(-3, 0, 0), Quaternion.identity, 0);
+            Debug.Log("GameInfo: Instantiate Player 2 Successfully");
+        }
+    }
+
+
+}
